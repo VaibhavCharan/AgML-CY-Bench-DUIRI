8909459a0ea30b88c867f6f8cb184eaf6e86d4a8
VaibhavCharan <vcharan@purdue.edu>
2025-07-05 21:26:51 -0700

    extract_results file

---
+++ misc/extract_results.py
@@ -0,0 +1,10 @@
+from cybench.runs.run_benchmark import compute_metrics
+
+run_name = "maize_Indiana" 
+df_metrics = compute_metrics(run_name)
+
+print(
+    df_metrics.groupby("model").agg(
+        {"normalized_rmse": "mean", "mape": "mean", "r2": "mean"}
+    )
+)
